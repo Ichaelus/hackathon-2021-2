1da537e50d68cff9ebb175715ffae686a5eaf71d
Stefan Langenmaier <stefan.langenmaier@makandra.de>
2021-11-13 13:42:54 +0100

    add sections

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,47 @@
 <script>
-	function convertImgToCanvas(){
+	function convertImgToCanvas() {
 		const srcImage = document.getElementById("srcImage");
 
-		srcImage.onload = function() {
-      const convertedImage = document.getElementById("convertedImage");
-      const context = convertedImage.getContext('2d');
+		srcImage.onload = function () {
+			console.log("")
+			const convertedImage = document.getElementById("convertedImage");
+			const context = convertedImage.getContext("2d");
 
-      context.drawImage(srcImage, 0, 0);
-    };
+			context.drawImage(srcImage, 0, 0);
+		};
 	}
 
-  document.addEventListener("DOMContentLoaded", convertImgToCanvas);
+	function compress() {}
+
+	function decompress() {}
+
+	document.addEventListener("DOMContentLoaded", convertImgToCanvas);
 </script>
 
 <main>
 	<h1>Hackathon 2021</h1>
-	<img id="srcImage" src="images/monkey.gif" alt="">
-	<canvas id="convertedImage" width="256" height="256"></canvas>
+
+	<div>
+		<h2>Source</h2>
+		<img id="srcImage" src="images/monkey.gif" alt="" />
+	</div>
+
+	<div>
+		<h2>Compress</h2>
+		<canvas id="convertedImage" width="256" height="256" />
+		<button on:click={compress}> Compress </button>
+	</div>
+
+	<div>
+		<h2>FIF</h2>
+		<textarea placeholder="Insert compressed image data"></textarea>
+	</div>
+
+	<div>
+		<h2>Decompress</h2>
+		<canvas id="decompressedImage" width="256" height="256" />
+		<button on:click={decompress}> Decompress </button>
+	</div>
 </main>
 
 <style>
